5ab01422c03f5ab9f9424a7b0684b3bdd1fae423
Hall Liu <hallliu000@gmail.com>
2015-04-19 22:40:58 -0500

    moving files and restructuring

--- scav_board/static/scav_board/scripts/views.js
+++ scav_board/static/scav_board/scripts/views.js
@@ -1,47 +1,5 @@
 "use strict";
 
-var app = app || {}
-
-var Comment = Backbone.Model.extend({
-    defaults: {
-        "text": "default comment text",
-        "author": "default author",
-        "datetime": new Date()
-    }
-});
-
-var ThreadHeader = Backbone.Model.extend({
-    defaults: {
-        "title": "Placeholder title",
-        "text": "Placeholder text"
-    }
-});
-
-var CommentCollection = Backbone.Collection.extend({
-    model: Comment,
-
-    getLastTwo: function() {
-        return this.models.slice(-2);
-    },
-
-    comparator: function(comment) {
-        return comment.get('datetime');
-    }
-});
-
-var CommentThread = Backbone.Model.extend({
-    defaults: {
-        "header": new ThreadHeader(),
-        "comments": new CommentCollection(),
-        "itemNumber": -1,
-        "numNew": 2
-    }
-});
-
-var ThreadCollection = Backbone.Collection.extend({
-    model: CommentThread,
-});
-
 var pageView = Backbone.View.extend({
     tagname: "ul",
     classname: "list-group",
