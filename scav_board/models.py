e4fe044cf75c2173f5d8f0ba0ce28fa305254e62
Hall Liu <hallliu000@gmail.com>
2015-04-19 23:08:32 -0500

    gitignore change and filling in models

--- scav_board/models.py
+++ scav_board/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=64, db_index=True, unique=True)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+
+class Item(models.Model):
+    number = models.IntegerField(db_index=True, unique=True)
+    page = models.IntegerField(db_index=True)
+    description = models.TextField()
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User)
+    timestamp = models.DateTimeField()
+    item = models.ForeignKey(Item, db_index=True)
+
