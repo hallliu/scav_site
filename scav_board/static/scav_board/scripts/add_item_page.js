8e3429e3b74fb85cefb53b095114626ebce6c6ac
Hall Liu <hallliu000@gmail.com>
2015-05-06 00:17:10 -0500

    fixed form clearing code

--- scav_board/static/scav_board/scripts/add_item_page.js
+++ scav_board/static/scav_board/scripts/add_item_page.js
@@ -10,19 +10,11 @@ var submit_fn = function() {
         success: function(data) {
             var num = data["item_number"];
             $("#submit-status").html("Item " + num + " has been successfully submitted.");
-            clear_fields(fields);
+            $("#item-form")[0].reset();
         },
         error: function(xhr) {
             $("#submit-status").html(xhr.responseText);
         }
     };
     $.ajax("/scav_board/add_item/", options);
-};
-
-var clear_fields = function(fields) {
-    for (var key in fields) {
-        if (fields.hasOwnProperty(key))
-            fields[key].val('');
-    }
-    fields["item-expires"].prop("checked", false);
 };
