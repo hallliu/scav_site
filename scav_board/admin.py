06e5e06b3798632d1fb22b968ba266aedfba8ee1
hallliu <hallliu000@gmail.com>
2015-05-06 21:48:27 -0500

    search in item admin

--- scav_board/admin.py
+++ scav_board/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import *
 
+
+class ItemAdmin(admin.ModelAdmin):
+    search_fields = ['description']
+
+
 admin.site.register(ItemCategory)
-admin.site.register(Item)
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Comment)
